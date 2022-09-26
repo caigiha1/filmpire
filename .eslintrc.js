1b7d842b3d25a9225f564377a7cef57a4aeea8d1
cagiha1 <tranminhquyet13@gmail.com>
2022-09-26 22:57:23 +0700

    file and folder structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,19 +8,22 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: '12',
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
+    'max-len': 0,
+    'no-param-reassign': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'react/function-component-definition': 0,
     'react/react-in-jsx-scope': 0,
-    'arrow-body-style': 0,
     'import/extensions': 0,
     'react/prop-types': 0,
     'linebreak-style': 0,
     'react/state-in-constructor': 0,
     'import/prefer-default-export': 0,
-    'max-len': [2, 250],
+    'arrow-body-style': ['error', 'always'],
     'no-multiple-empty-lines': [
       'error',
       {
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
-  return <div>App</div>;
-}
+import useStyles from './styles';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
 export default App;
