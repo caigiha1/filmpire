4b6bc75707c71ed9b641780ba6138b56ca3f5a4d
cagiha1 <tranminhquyet13@gmail.com>
2022-09-27 00:45:08 +0700

    style navbar component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
 
+const theme = createTheme({});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>,
 );
